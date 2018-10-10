9f3262d522539d4df84e5edac948c0a1155aa189
FreeDom <theflashmaybe0728@gmail.com>
2018-10-09 21:13:49 -0400

    Commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const botconfig = require("./botconfig.json");
-const tokenfile = require("./token.json");
+const token = precess.env.token;
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
